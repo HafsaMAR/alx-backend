c105927587669d2a2e7bffaf295200653dfe6cd6
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-03 09:00:38 +0000

    BasicCache class functions

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,16 +10,18 @@ class BasicCache(BaseCaching):
     """ Class to implement the put and get functions
         to populate the cache_data dictionary
     """
-    
+
     def put(self, key, item):
         """ Puts elements in the cache_data dictionary
         """
+        
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
         """ Retrieves the value stored in the cache_data dict
         """
+
         if key not in self.cache_data or key is None:
             return None
         return self.cache_data[key]
