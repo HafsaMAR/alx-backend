98ef4aff261b39ebad6f1f96ed3c5321d12b05b4
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-09 09:03:29 +0000

    pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """render template index.html"""
     return render_template("0-index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,21 +7,27 @@ from flask_babel import Babel
 import babel
 
 
-
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
+    """
+    Config class
+    """
     LANGUAGES = ['en', 'fr']
     TIME_ZONE = "UTC"
     DEFAULT_LANGUAGE = 'en'
 
+
 app.config.from_object(Config)
+
 
 @app.route("/")
 def index():
     """render template index.html"""
     return render_template("1-index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
