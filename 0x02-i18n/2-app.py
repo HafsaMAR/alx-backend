a76c54ccf3e02ee23d2a6b50367983365895446e
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-09 09:04:56 +0000

    get_locale documentation

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,6 +25,9 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """
+    get_local language
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
