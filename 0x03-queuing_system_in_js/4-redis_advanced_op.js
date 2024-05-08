1e326046fdb36ff619e173635f88fc39f59f5eae
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-08 13:21:06 +0100

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+const client = redis.createClient();
+
+// connect to the Redis server
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.error('Redis client not connected to the server:', err)
+});
+
+function createHash() {
+    client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+    client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+    client.hset('HolbertonSchools', 'New York', 20, redis.print);
+    client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+    client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+    client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+function displayHash() {
+    client.hGetAll('HolbertonSchools', (err, reply) => {
+        if (err) {
+            console.error(err)
+        } else  {
+            console.log('Hash stored in Redis:', reply);
+        }
+    })
+}
