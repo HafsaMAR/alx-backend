0e76809b4873336a7b26bbab26b2365f6aebd00a
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-08 15:20:44 +0100

    Track progress and errors with Kue

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue({concurrency: 2});
+
+function sendNotification(phoneNumber, message, job, done) {
+    job.progress(0, 100);
+    
+    if (blacklistedNumbers.includes(phoneNumber)) {
+      const error = new Error(`Phone number ${phoneNumber} is blacklisted`);
+      done(error);
+    } else {
+      // Track the progress to 50%
+      job.progress(50, 100);
+      
+
+      console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+      done();
+    }
+  }
+  
+  // Process jobs in the 'push_notification_code_2' queue
+  queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+
+    sendNotification(phoneNumber, message, job, done);
+  });
