3c4007812007a4d588439b5149338c0b77f6e526
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-08 13:04:41 +0100

    task 0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+
+import redis from 'redis'
+
+const client = redis.createClient();
+
+// Event listeners for Redis client
+
+redis.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
+redis.on('error', (error) => {
+    console.error(`Redis client not connected to the server: ${error.message}`);
+});
