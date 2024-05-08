14bc1c64505e4342ec73dbb487fd1f8a2f8015f4
HafsaMAR <marouche.hafsa@gmail.com>
2024-05-08 15:05:00 +0100

    Create the Job processor and creator

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '1234567890',
+    message: 'Hello, world'
+};
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+    if (err) {
+        console.error('Error creating job:', err);
+    } else {
+        console.log('Notification job created:', job.id)
+    }
+});
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+    console.log('notification job failed');
+});
